ce571885d13e21925e85d1902ecf1dcd0ee08955
'RajanXVII' <a839348@DESKTOP-DN1RNVP>
2022-06-07 18:42:30 +0530

    Enabling https

--- src/main/java/com/example/demo/SpringSecurity1Application.java
+++ src/main/java/com/example/demo/SpringSecurity1Application.java
@@ -2,13 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
-public class SpringSecurity1Application {
+@RestController
+public class SpringSecurity1Application extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().permitAll();
+	}
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurity1Application.class, args);
 	}
@@ -17,10 +27,7 @@ public class SpringSecurity1Application {
 	public String home() {
 		return "home";
 	}
-	@GetMapping("/login")
-	public String login() {
-		return "login";
-	}
+	
 	
 
 }
